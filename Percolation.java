af94bb782acd034f42703f0ced0e5eaf7636d34a
tristanjlang <tristan@alum.mit.edu>
2014-02-08 15:57:21 -0500

    completed Percolation and PercolationStats, but Percolation still has backwash

--- Percolation.java
+++ Percolation.java
@@ -1,26 +1,71 @@
 public class Percolation {
-   private int[][] grid;
-   private int gridsize;
-   
-   // create N-by-N grid, with all sites blocked
-   public Percolation(int N) {
-       this.grid = new int[N][N];
-       this.gridsize = N*N;
-   }
-   
-   public void open(int i, int j)         // open site (row i, column j) if it is not already
-       // union
-       
-   public boolean isOpen(int i, int j)    // is site (row i, column j) open?
-       
-       
-   public boolean isFull(int i, int j)    // is site (row i, column j) full?...does that site touch the top?
-       // connected (top to i,j)
-       
-   public boolean percolates()            // does the system percolate?
-       // connected (top to bottom)
+    private int[][] grid; // stores 0 and 1 to show whether a site is open
+    private int gridsize; // length of one side of the square grid
+    private WeightedQuickUnionUF sites; // the connectedness of the cells in the grid
+    
+    // create N-by-N grid, with all sites blocked
+    public Percolation(int N) {
+        this.grid = new int[N][N];
+        this.gridsize = N;
+        this.sites = new WeightedQuickUnionUF(N*N + 2); // +2 are virtual start/end
+    }
+    
+    // return the 1D representation of the point (i,j)
+    private int xyTo1D(int i, int j)
+    { return (i - 1) + (j - 1) * this.gridsize + 1; }
+    
+    // throw exception of i or j is outside of the specified bounds
+    private void validateIndices(int i, int j) {
+        if (i <= 0 || i > this.gridsize) {
+            throw new IndexOutOfBoundsException("row index " + i + " out of bounds");
+        }
+        if (j <= 0 || j > this.gridsize) {
+            throw new IndexOutOfBoundsException("col index " + j + " out of bounds");
+        }
+    }
+    
+    // opens site (row i, column j) if it is not already open
+    // connects it to its open neighbors
+    public void open(int i, int j) {
+        validateIndices(i, j);
+        if (!isOpen(i, j)) {
+            this.grid[i - 1][j - 1] = 1;
+            
+            // if in the first/last row, connect to virtual start/end, respectively
+            if (i == 1) sites.union(0, xyTo1D(i, j));
+            if (i == this.gridsize) {
+                sites.union(xyTo1D(i, j), this.gridsize * this.gridsize + 1);
+            }
+            
+            // connect to other open neighbors
+            if (i > 1 && isOpen(i - 1, j)) {
+                sites.union(xyTo1D(i - 1, j), xyTo1D(i, j));
+            }
+            if (i < this.gridsize && isOpen(i + 1, j)) {
+                sites.union(xyTo1D(i + 1, j), xyTo1D(i, j));
+            }
+            if (j > 1 && isOpen(i, j - 1)) {
+                sites.union(xyTo1D(i, j - 1), xyTo1D(i, j));
+            }
+            if (j < this.gridsize && isOpen(i, j + 1)) {
+                sites.union(xyTo1D(i, j + 1), xyTo1D(i, j));
+            }
+        }
+    }
+    
+    // is site (row i, column j) open?
+    public boolean isOpen(int i, int j) {
+        validateIndices(i, j);
+        return (this.grid[i - 1][j - 1] == 1);
+    }
+    
+    // is site (row i, column j) full?...does that site touch the top?
+    public boolean isFull(int i, int j) {
+        validateIndices(i, j);
+        return this.sites.connected(xyTo1D(i, j), 0);
+    }
+    
+    // does the system percolate?
+    public boolean percolates()
+    { return this.sites.connected(0, this.gridsize * this.gridsize + 1); }
 }
-
-// void union - merge component containing site 1 to component containing site 2
-// boolean connected - are two sites in the same component?
-// int find - returns component identifier (root)
---
+++ PercolationStats.java
@@ -0,0 +1,61 @@
+public class PercolationStats {
+    private int N;
+    private int T;
+    private double[] sitesRequired;
+    
+    // perform T independent computational experiments on an N-by-N grid
+    public PercolationStats(int N, int T) {
+        if (N <= 0) { throw new IllegalArgumentException("N must be > 0"); }
+        if (T <= 0) { throw new IllegalArgumentException("T must be > 0"); }
+        this.N = N;
+        this.T = T;
+        this.sitesRequired = new double[T];
+    }
+    
+    // adds an entry to the array of the ratio of sites required
+    private void addToSitesRequired(int index, int numsites)
+    { this.sitesRequired[index] = (double) numsites / (double) (N * N); }
+    
+    // sample mean of percolation threshold
+    public double mean()
+    { return StdStats.mean(sitesRequired); }
+    
+    // sample standard deviation of percolation threshold
+    public double stddev()
+    { return StdStats.stddev(sitesRequired); }
+    
+    // returns lower bound of the 95% confidence interval
+    public double confidenceLo()
+    { return mean() - 1.96 * stddev() / Math.sqrt(T); }
+    
+    // returns upper bound of the 95% confidence interval
+    public double confidenceHi()
+    { return mean() + 1.96 * stddev() / Math.sqrt(T); }        
+    
+    public static void main(String [] args) {
+        PercolationStats ps = new PercolationStats(N, T);
+        
+        for (int i = 0; i < T; i++) {
+            Percolation p = new Percolation(N);
+            int opencount = 0;
+            
+            while (!p.percolates()) {
+                int row, col;
+                do {
+                    row = StdRandom.uniform(1, N + 1);
+                    col = StdRandom.uniform(1, N + 1);
+                } while (p.isOpen(row, col));
+            
+                p.open(row, col);
+                opencount++;
+            }
+            
+            ps.addToSitesRequired(i, opencount);
+        }
+        
+        StdOut.println("mean\t\t\t" + "= " + ps.mean());
+        StdOut.println("stddev\t\t\t" + "= " + ps.stddev());
+        StdOut.println("95% confidence interval\t" + "= " + ps.confidenceLo()
+                           + ", " + ps.confidenceHi());
+    }
+}
